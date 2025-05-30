756a379ac8898871d5f00a8e366097a000796be5
Imam Hossain <iimamhossain23@gmail.com>
2025-05-30 17:10:10 +0600

    Remove TopBarWidgetMobile from DashboardWidgetMobile; streamline layout for improved performance

--- lib/features/dashboard/dashboard_widget_mobile.dart
+++ lib/features/dashboard/dashboard_widget_mobile.dart
@@ -24,7 +24,6 @@ class DashboardWidgetMobile extends StatelessWidget {
     final scrollController = getIt<ScrollController>();
 
     return Scaffold(
-      appBar: const TopBarWidgetMobile(),
       endDrawer: const AppDrawerWidget(),
       body: SafeArea(
         child: _buildDashboardBody(
