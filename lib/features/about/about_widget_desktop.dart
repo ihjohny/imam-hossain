eecfc35c3f9a8c4d4ee26d8d2801d05dd534a2b2
Imam Hossain <iimamhossain23@gmail.com>
2025-06-06 12:08:21 +0600

    Refactor AboutWidgetDesktop and AppCursorsButtonWidget to use AppSelectionAreaWidget for improved selection handling

--- lib/features/about/about_widget_desktop.dart
+++ lib/features/about/about_widget_desktop.dart
@@ -6,8 +6,10 @@ import 'package:imam_hossain/features/about/data/model/about_data.dart';
 import 'package:imam_hossain/features/common/widgets/app_card_widget.dart';
 import 'package:imam_hossain/features/common/widgets/app_no_data_widget.dart';
 import 'package:imam_hossain/features/common/widgets/app_section_widget.dart';
+import 'package:imam_hossain/features/common/widgets/app_selection_area_widget.dart';
 import 'package:imam_hossain/features/common/widgets/app_tag_chip_widget.dart';
 import 'package:imam_hossain/features/common/widgets/app_wrap_widget.dart';
+
 import '../../core/utils/constants/sizes.dart';
 import '../../di/di_setup.dart';
 import '../../generated/localization/locale_keys.g.dart';
@@ -58,10 +60,12 @@ class AboutWidgetDesktop extends StatelessWidget {
   }
 
   Widget _buildAboutDetails(BuildContext context, String? details) {
-    return Text(
-      details ?? "",
-      style: context.themeData.textTheme.titleMedium?.copyWith(
-        color: context.themeData.colorScheme.onSurfaceVariant,
+    return AppSelectionAreaWidget(
+      child: Text(
+        details ?? "",
+        style: context.themeData.textTheme.titleMedium?.copyWith(
+          color: context.themeData.colorScheme.onSurfaceVariant,
+        ),
       ),
     );
   }
--- lib/features/common/widgets/app_cursors_button_widget.dart
+++ lib/features/common/widgets/app_cursors_button_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imam_hossain/features/common/widgets/app_selection_area_widget.dart';
 
 class AppCursorsButtonWidget extends StatelessWidget {
   final VoidCallback? onPressed;
@@ -18,7 +19,12 @@ class AppCursorsButtonWidget extends StatelessWidget {
           : SystemMouseCursors.basic,
       child: GestureDetector(
         onTap: onPressed ?? () {},
-        child: child,
+        child: AppSelectionAreaWidget(
+          mouseCursor: onPressed != null
+              ? SystemMouseCursors.click
+              : SystemMouseCursors.basic,
+          child: child,
+        ),
       ),
     );
   }
