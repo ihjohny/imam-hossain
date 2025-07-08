8f54bfadc3fac60354010b7f681edd7ed5505173
ihjohny <ihjohny@users.noreply.github.com>
2025-07-08 19:38:50 +0000

    deploy: 4b7eeafd337204a1fb5e2848279d251fb8fc118e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "57499580"
+    serviceWorkerVersion: "1052367867"
   }
 });
